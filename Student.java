33eb8f0ea8250fd1af3a0d68180b7bb7a31c03ac
Dimitar Papalazov <d.papalazov@outlook.com>
2020-03-30 15:35:10 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -1,18 +1,66 @@
+import java.util.List;
+
 class Student {
-	String index;
-	String firstName;
-	String lastName;
-	List<Integer> grades;
+    String index;
+    String firstName;
+    String lastName;
+    List<Integer> grades;
 
-	//TODO constructor
+    public Student(String index, String firstName, String lastName, List<Integer> grades) {
+        this.index = index;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grades = grades;
+    }
 
-	//TODO seters & getters
+    public String getIndex() {
+        return index;
+    }
 
-	public double getAverage() {
-		//TODO
-	}
+    public void setIndex(String index) {
+        this.index = index;
+    }
 
-	public int ECTSCredits() {
-		//TODO
-	}
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Integer> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(List<Integer> grades) {
+        this.grades = grades;
+    }
+
+    public double getAverage() {
+        int sum = 0;
+        for (Integer i : grades) {
+            sum += i;
+        }
+
+        return (double) (sum / grades.size());
+    }
+
+    public int ECTSCredits() {
+        int count = 0;
+        for (Integer i : grades) {
+            if (i > 5 && i <= 10) {
+                count++;
+            }
+        }
+        return count * 6;
+    }
 }
